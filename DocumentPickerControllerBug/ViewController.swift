9a0c1cb29304de61cebd3bbabdbf6f112c5988e7
Yusuf Miletli <yusuf.miletli@check24.de>
2025-02-06 09:51:47 +0100

    Initial Commit

---
+++ DocumentPickerControllerBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocumentPickerControllerBug
+//
+//  Created by Yusuf Temel Miletli on 06.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
